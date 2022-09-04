6b99650bc974423814b5320c1c29d33f8922b57b
Joff Tiquez <jofftiquez@gmail.com>
2022-09-04 18:11:53 +0800

    refactor: add new page for The Team

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,6 +8,14 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') },
     ],
   },
+  {
+    path: '/team',
+    name: 'team',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/TeamPage.vue') },
+    ],
+  },
 
   // Always leave this as last one,
   // but you can also remove it

299c12958761ae32d3e758b3a574285d0183e078
Joff Tiquez <jofftiquez@gmail.com>
2022-09-06 20:24:56 +0800

    refactor: update staging link deploy and ids

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,5 +1,5 @@
 firebase use ossph-org && \
 firebase deploy --only functions:ossphSSRHandler && \
 firebase target:clear hosting staging && \
-firebase target:apply hosting staging ossph-org && \
+firebase target:apply hosting staging staging-ossph-org && \
 firebase deploy --only hosting:staging

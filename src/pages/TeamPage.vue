5c63413edcb377aacf56c594ba2d64dc9009dd21
Joff Tiquez <jofftiquez@gmail.com>
2022-09-09 11:36:36 +0800

    refactor: update team page
    - add team member profiles

--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -1,22 +1,55 @@
 <template lang="pug">
 generic-panel(
-  :panel-classes="['items-center', 'grey-panel', 'bg-transparent']"
+  :panel-classes="['grey-panel', 'bg-transparent']"
+  padding-top="0px"
+  padding-top-mobile="0px"
   hide-hr
 )
   div.row.items-center.justify-center
-    div.col-xs-12.col-md-8
-      h1.text-h2.text-primary The Team #[span.text-h5 (Coming Soon)]
-      //- p.text-justify.text-body1 Open Source Software PH (OSSPH) is a developer-led initiative to grow the community of developers building open source software across the Philippines. Our goal is to help open source maintainers to get proper support for their projects. Our future plans include virtual meetups, training for new OSS maintainers, connecting them with mentors and companies that can potentially support them.
-    div.col-xs-12.col-md-8.q-py-lg
-      q-btn(
-        label="Join Our Team!"
-        size="lg"
-        color="primary"
-        no-caps
-        unelevated
-        href="https://forms.gle/MjHy9WmB7cEeXDVV9"
-        target="_blank"
-      )
+    div.col-xs-12.col-md-8.text-center
+      h1.text-weight-medium.text-h2.text-primary The OSSPH Team
+      p.text-body1.q-mb-xl Here are the amazing people behind OSSPH's initiatives
+      div.row.wrap.justify-center
+        template(v-for="team in theTeam")
+          div.col-xs-12.col-md-3.q-pa-md.text-center
+            q-img(
+              :src="team.photo"
+            ).q-mb-md
+            div
+              span.text-h4 {{team.name}}
+            div
+              span.text-body1 {{team.role}}
+            div
+              q-btn(
+                label="Connect"
+                color="primary"
+                flat
+                no-caps
+              ).full-width
+                q-menu(fit)
+                  q-list
+                    template(v-for="social in team.socials")
+                      q-item(
+                        target="_blank"
+                        v-close-popup
+                        clickable
+                        :href="social.link"
+                      )
+                        //- q-item-section(avatar)
+                        //-   q-icon(:name="social.icon")
+                        q-item-section {{social.name}}
+
+      div.row.wrap.justify-center
+        div.col-xs-12.q-mt-lg.text-center
+          q-btn(
+            label="Join Our Team!"
+            size="lg"
+            color="primary"
+            no-caps
+            unelevated
+            href="https://forms.gle/MjHy9WmB7cEeXDVV9"
+            target="_blank"
+          ).q-mb-xl
 </template>
 
 <script>
@@ -33,6 +66,66 @@ export default {
     const smoothScroll = inject('smoothScroll');
     const $q = useQuasar();
     const isMobile = computed(() => $q.screen.lt.md);
+    const theTeam = [
+      {
+        name: 'Joff',
+        role: 'Founder',
+        photo: require('@/assets/images/joff.png'),
+        socials: [
+          {
+            name: 'GitHub',
+            icon: 'github',
+            link: 'https://github.com/jofftiquez',
+          },
+          {
+            name: 'Twitter',
+            icon: 'twitter',
+            link: 'https://twitter.com/jrtiquez',
+          },
+          {
+            name: 'LinkedIn',
+            icon: 'linkedin',
+            link: 'https://linkedin.com/in/jofftiquez',
+          },
+        ],
+      },
+      {
+        name: 'Waren',
+        role: 'Community Leader',
+        photo: require('@/assets/images/waren.png'),
+        socials: [
+          {
+            name: 'Twitter',
+            icon: 'twitter',
+            link: 'https://twitter.com/warengonzaga',
+          },
+        ],
+      },
+      {
+        name: 'Avie',
+        role: 'Community Leader',
+        photo: require('@/assets/images/avie.png'),
+        socials: [
+          {
+            name: 'Twitter',
+            icon: 'twitter',
+            link: 'https://twitter.com/AvieDev',
+          },
+        ],
+      },
+      {
+        name: 'Benj',
+        role: 'Head Artist',
+        photo: require('@/assets/images/benj.png'),
+        socials: [
+          {
+            name: 'Twitter',
+            icon: 'twitter',
+            link: 'https://twitter.com/annabenjamine',
+          },
+        ],
+      },
+    ];
 
     function onGoToPanel (card) {
       const panelId = card.panelId;
@@ -50,6 +143,7 @@ export default {
 
     return {
       isMobile,
+      theTeam,
       onGoToPanel,
     };
   },

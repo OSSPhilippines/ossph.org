81deb8301626d71909c959ba780672bae2a9948d
Joff Tiquez <jofftiquez@gmail.com>
2022-09-13 18:09:20 +0800

    refactor: add new child route

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,7 @@ const routes = [
     children: [
       { path: '', name: 'home', component: () => import('pages/IndexPage.vue') },
       { path: 'team', name: 'team', component: () => import('pages/TeamPage.vue') },
+      { path: 'projects', name: 'projects', component: () => import('pages/ProjectsPage.vue') },
     ],
   },
 

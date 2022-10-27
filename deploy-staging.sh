3aae765a497d5b54373e0f42aa3fd57406aff1f6
Joff Tiquez <jofftiquez@gmail.com>
2022-10-27 20:29:18 +0800

    chore: update deploy script
    - remove redundant script
    - rename script to just deploy.sh
    - update package.json script for deployment

--- deploy-staging.sh
+++
@@ -1,5 +0,0 @@
-firebase use ossph-org && \
-firebase deploy --only functions:ossphSSRHandler && \
-firebase target:clear hosting staging && \
-firebase target:apply hosting staging staging-ossph-org && \
-firebase deploy --only hosting:staging

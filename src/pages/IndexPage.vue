df4690dc80c87f5466b5ca463d6f2ae7c3b7033c
fidellim <lim.fidel@gmail.com>
2023-04-23 11:16:42 +0400

    chore: use pug style class

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -216,8 +216,7 @@ generic-panel(
         target="_blank"
         no-caps
         unelevated
-        class="btn-contribute"
-      )
+      ).btn-contribute
 //- div.col-xs-12.col-md-6.text-center
 //-   //- p.text-justify.ossph-text-paragraph Open Source Software PH (OSSPH) is a developer-led initiative to grow the community of developers building open source software across the Philippines. Our goal is to help open source maintainers to get proper support for their projects. Our future plans include virtual meetups, training for new OSS maintainers, connecting them with mentors and companies that can potentially support them.
 //-   //- p.text-justify.ossph-text-paragraph Open Source Software PH (OSSPH) is a developer-led initiative to grow the community of developers building open source software across the Philippines.

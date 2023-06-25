588246ab0118db25af4f20b93cbc38d7d94112d8
Joff Tiquez <jofftiquez@gmail.com>
2023-06-25 22:48:52 +0800

    🎨 style(index.vue): add generic panels and update layout ✨ feat(index.vue): add GenericPanel component The layout of the page has been updated to use the default layout. The page now uses the GenericPanel component to display content in a more organized manner. The component is used to display information about the community partners, sponsors, active community, learning opportunities, and contribution and collaboration.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,87 @@
 
 <template>
-  <div>
-    <h1 class="">Home</h1>
+
+<generic-panel class="bg-red-50">
+  <div class="grid grid-cols-2">
+    <div>
+      <h1>Open Source Software PH</h1>
+      <h2>The leading Open Source Software Community in the Philippines</h2>
+      <button class="btn btn-primary">Join The Movement</button>
+      <button class="btn btn-primary">Help Us Grow</button>
+    </div>
+    <div>
+      <img src="https://loremflickr.com/640/480/nightlife" alt="Night Life">
+    </div>
   </div>
+</generic-panel>
+
+<generic-panel class="bg-green-50">
+  <div class="flex flex-col">
+    <h1>Our Community Partners 💪</h1>
+    <div class="grid grid-cols-5">
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+    </div>
+  </div>
+</generic-panel>
+
+<generic-panel class="bg-blue-50">
+  <div class="flex flex-col">
+    <h1>Our Sponsors 😇</h1>
+    <div class="grid grid-cols-4">
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+    </div>
+  </div>
+</generic-panel>
+
+<generic-panel>
+  <div class="flex flex-col">
+    <h1>Active Community 🎉</h1>
+    <div class="grid grid-cols-2">
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+    </div>
+  </div>
+</generic-panel>
+
+<generic-panel>
+  <div class="flex flex-col">
+    <h1>Learn Regularly 💡</h1>
+    <div class="grid grid-cols-2">
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+    </div>
+  </div>
+</generic-panel>
+
+<generic-panel>
+  <div class="flex flex-col">
+    <h1>Contribution & Collaboration 🍺</h1>
+    <div class="grid grid-cols-2">
+      <i class="las la-otter"></i>
+      <i class="las la-otter"></i>
+    </div>
+  </div>
+</generic-panel>
 </template>
 
 <script>
 import { definePageMeta } from '#imports';
+import GenericPanel from '@/components/commons/GenericPanel.vue';
 
 export default {
+  components: {
+    GenericPanel,
+  },
   setup () {
     definePageMeta({
-      layout: 'blank',
+      layout: 'default',
     });
   },
 };

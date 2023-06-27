aa7603bcdbe32811470db004671ce7da2683da0f
Daniel Padilla <danielfpadilla@gmail.com>
2023-06-27 22:21:41 +0800

    feat(Footer.vue): add Footer.vue

---
+++ src/components/commons/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+<footer class="footer footer-center p-10 text-primary-content ossph-gradient-bg">
+  <div class="grid grid-flow-col gap-4">
+    <a href="https://discord.com/servers/open-source-software-ph-905496362982981723"
+      target="_blank">
+      <button class="btn btn-outline text-white">
+        <i class="lab la-discord"></i>
+        Discord
+      </button>
+    </a>
+    <a href="https://www.facebook.com/ossph.org"
+      target="_blank">
+      <button class="btn btn-outline text-white">
+        <i class="lab la-facebook"></i>
+        Facebook
+      </button>
+    </a>
+    <a href="https://twitter.com/OSSPhilippines"
+      target="_blank">
+      <button class="btn btn-outline text-white">
+        <i class="lab la-twitter"></i>
+        Twitter
+      </button>
+    </a>
+    <a href="https://bit.ly/3xjDvN2"
+      target="_blank">
+      <button class="btn btn-outline text-white">
+        <i class="la la-newspaper"></i>
+        Press Kit
+      </button>
+    </a>
+  </div>
+  <p class="">Copyright © 2023 Open Source Software PH. All Rights Reserved.</p>
+</footer>
+</template>
+
+<style scope>
+footer {
+  height: 200px;
+}
+</style>

ca435f6b417444f3bc51869968ba91dc1b353ad0
Daniel Padilla <danielfpadilla@gmail.com>
2023-06-28 11:11:27 +0800

    feat: apply button style to "btn btn-ghost"

--- src/components/commons/Footer.vue
+++ src/components/commons/Footer.vue
@@ -3,28 +3,28 @@
   <div class="grid grid-flow-col gap-4">
     <a href="https://discord.com/servers/open-source-software-ph-905496362982981723"
       target="_blank">
-      <button class="btn btn-outline text-white">
+      <button class="btn btn-ghost text-white">
         <i class="lab la-discord"></i>
         Discord
       </button>
     </a>
     <a href="https://www.facebook.com/ossph.org"
       target="_blank">
-      <button class="btn btn-outline text-white">
+      <button class="btn btn-ghost text-white">
         <i class="lab la-facebook"></i>
         Facebook
       </button>
     </a>
     <a href="https://twitter.com/OSSPhilippines"
       target="_blank">
-      <button class="btn btn-outline text-white">
+      <button class="btn btn-ghost text-white">
         <i class="lab la-twitter"></i>
         Twitter
       </button>
     </a>
     <a href="https://bit.ly/3xjDvN2"
       target="_blank">
-      <button class="btn btn-outline text-white">
+      <button class="btn btn-ghost text-white">
         <i class="la la-newspaper"></i>
         Press Kit
       </button>

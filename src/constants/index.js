7de6bbaeaf933bc751615853a17553bbddae4282
Daniel Padilla <danielfpadilla@gmail.com>
2023-07-04 19:33:18 +0800

    feat: add constants

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export default {
+  TITLE_DEFAULT: 'Open Source Software PH (OSSPH)',
+  DESCRIPTION_DEFAULT: 'Open Source Software PH (OSSPH) is a developer-led initiative to grow the community of developers building open source software across the Philippines.',
+};

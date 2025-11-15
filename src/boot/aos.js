4bd879f3f97236d1b181b7d0c6d22ac491222d2d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-11-15 14:24:21 +0800

    refactor(aos.js, vue-smooth-scroll.js, IndexPage.vue): Simplify AOS initialization and remove delays
    Refactor AOS initialization in `aos.js` to remove client-side checks and streamline configuration. Update `IndexPage.vue` to eliminate AOS delay attributes for a more uniform animation effect.
    
    Changes:
    - Simplified AOS initialization in `aos.js`
    - Removed delay attributes from AOS elements in `IndexPage.vue` for consistent animation timing

--- src/boot/aos.js
+++ src/boot/aos.js
@@ -1,7 +1,9 @@
 import { boot } from 'quasar/wrappers';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-export default boot(({ app }) => {
-  AOS.init();
+
+export default boot(() => {
+  AOS.init({
+    once: true, // Don't trigger animations on scroll back up
+  });
 });
--- src/boot/vue-smooth-scroll.js
+++ src/boot/vue-smooth-scroll.js
@@ -1,4 +1,6 @@
+import { boot } from 'quasar/wrappers';
 import VueSmoothScroll from 'vue3-smooth-scroll';
-export default ({ app }) => {
+
+export default boot(({ app }) => {
   app.use(VueSmoothScroll);
-};
+});
